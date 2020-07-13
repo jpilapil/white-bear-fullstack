65771881d18db36dba509b26b4f08084704fcd3f
Justin Pilapil <57848030+jpilapil@users.noreply.github.com>
2020-07-13 15:42:55 -0700

    change query

--- server.js
+++ server.js
@@ -9,12 +9,27 @@ const connection = mysql.createConnection({
 
 connection.connect();
 
-connection.query("SELECT 1 + 1 AS solution", (error, results, fields) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log("The solution is: ", results[0].solution);
+connection.query(
+  `
+  SELECT 
+      *
+   FROM
+      memory_cards
+   WHERE
+      memory_cards.user_id = 'a70d566a-9701-449e-8556-554bfda5be2f'
+         AND (memory_cards.imagery LIKE '%ash%'
+         OR memory_cards.answer LIKE '%ash%')
+   ORDER BY 
+         memory_cards.total_successful_attempts DESC ,
+         memory_cards.created_at DESC;
+   `,
+  (err, res) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(res);
+    }
   }
-});
+);
 
 connection.end();
