2f153317d884bf1dc4617212bc061d7a6170ec8d
Justin Pilapil <57848030+jpilapil@users.noreply.github.com>
2020-07-15 10:56:32 -0700

    set proxy delete cors

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const app = express();
-var cors = require("cors");
 
-app.use(cors());
 app.use("/api/v1/users", require("./api/v1/users")); // use the route, require the file
 app.use("/api/v1/memory-cards", require("./api/v1/memory-cards"));
 app.get("/", (req, res) => res.send("Hello World!"));
