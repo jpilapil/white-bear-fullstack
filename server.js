484c2e660b62f91f4036fe89b565e56c3f1aa6f4
Justin Pilapil <57848030+jpilapil@users.noreply.github.com>
2020-08-21 17:20:22 -0700

    test

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use("/api/v1/memory-cards", require("./api/v1/memory-cards"));
 
 app.use(express.static("client/build"));
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname), "client", "build", "index.html"); // ./client/build/index.html
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html")); // ./client/build/index.html
 });
 
 const port = process.env.PORT || 3333;
